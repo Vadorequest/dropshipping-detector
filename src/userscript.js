60d0f9bb908dfce128437ebfa72a17d92f5a84bd
Ambroise Dhenain <ambroise.dhenain@gmail.com>
2024-10-06 11:57:38 +0200

    "s"

--- src/userscript.js
+++ src/userscript.js
@@ -146,8 +146,8 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${warningColor}" width="24px" height="24px" style="${iconStyle}">
           <path d="M0 0h24v24H0V0z" fill="none"/><path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>
         </svg>
-        <span>ATTENTION: Ce site a ${probability}% de probabilité d'être un site de DROPSHIPPING!</span>
-        <span style="font-style: italic">(${technos?.length || 0} technos | ${similarArticles?.length || 0} articles)</span>
+        <span>ATTENTION: Ce site a ${probability}% de probabilité d'être un site de DROPSHIPPING selon AntiDrop !</span>
+        <span style="font-style: italic">(${technos?.length || 0} techno${technos?.length > 1 ? 's' : ''} | ${similarArticles?.length || 0} article${similarArticles?.length > 1 ? 's' : ''})</span>
         <a href="#" id="viewDetails" style="text-decoration: underline; color: black; font-weight: bold; margin-left: 20px;">Voir les détails</a>
       </div>
 
@@ -211,7 +211,7 @@
       <div style="margin-bottom: 20px;">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red" width="48px" height="48px" style="${iconStyle}">
           <path d="M0 0h24v24H0V0z" fill="none"/><path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"/>
-        </svg> ATTENTION: Ce site a ${probability}% de probabilité d'être un site de DROPSHIPPING!
+        </svg> ATTENTION: Ce site a ${probability}% de probabilité d'être un site de DROPSHIPPING selon AntiDrop !
       </div>
 
       <!-- Technologies section -->
